ae978e544f4d3b4013719886d8e2f9b3c348381d
Krauchanka Aliaksandr <krav1983@tut.by>
2022-09-15 06:29:27 +0300

    2.1. Подвиг 8. Объявите в программе два класса Point и Rectangle

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-class Line:
-
-    def __init__(self, x1, y1, x2, y2):
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__x2 = x2
-        self.__y2 = y2
-
-    def set_coords(self, x1, y1, x2, y2):
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__x2 = x2
-        self.__y2 = y2
+class Point:
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
 
     def get_coords(self):
-        return (self.__x1, self.__y1, self.__x2, self.__y2)
+        return self.__x, self.__y
+
+class Rectangle:
+    def __init__(self, a, b, c=None, d=None):
+        self.__sp = self.__ep = None
+        if isinstance(a, Point) and isinstance(b, Point):
+            self.__sp = a
+            self.__ep = b
+        elif all(map(lambda x: type(x) in (int, float), (a,b,c,d))):
+            self.__sp = Point(a, b)
+            self.__ep = Point(c, d)
+
+    def set_coords(self, sp, ep):
+        self.__sp = sp
+        self.__ep = ep
+
+    def get_coords(self):
+        return self.__sp, self.__ep
 
     def draw(self):
-        print(self.__x1, self.__y1, self.__x2, self.__y2)
+        print(f'Прямоугольник с координатами: {self.__sp.get_coords()}, {self.__ep.get_coords()}')
+
+
+rect = Rectangle(0, 0, 20, 34)
