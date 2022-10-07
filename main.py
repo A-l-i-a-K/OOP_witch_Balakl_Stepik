5f880ced8aa1961c11e339b9aaa9b3e86553b1de
Krauchanka Aliaksandr <krav1983@tut.by>
2022-10-07 13:53:19 +0300

    Поиск ссылки в HTML

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-class RenderList:
-    def __init__(self, type_list):
-        self.type_list = type_list if type_list in ("ul","ol") else "ul"
+import re
 
-    def __call__(self, lst, *args, **kwargs):
-        return '*****'.join([f'<{self.type_list}>', *(map(lambda x:f'<li>{x}</li>', lst)), f'</{self.type_list}>'])
+st = input() # HTML
 
-lst = ["Пункт меню 1", "Пункт меню 2", "Пункт меню 3"]
-render = RenderList("ol")
-html = render(lst)
-print(html)
+res = re.findall(r'''<a\s[^>]+?href\s?=\s?['"]([^'"]*)['"]''', st)
+
+print(*res, sep='\n')
